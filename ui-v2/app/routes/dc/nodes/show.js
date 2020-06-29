5f8afb35daab4cbf325866331c38e69fa284f101
kenia <keniavalladarez@gmail.com>
2020-06-29 13:43:13 -0400

    Redesign: Service Instaces tab in for a Node

--- ui-v2/app/routes/dc/nodes/show.js
+++ ui-v2/app/routes/dc/nodes/show.js
@@ -1,13 +1,11 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { hash } from 'rsvp';
-import { get } from '@ember/object';
 
 export default Route.extend({
   repo: service('repository/node'),
   sessionRepo: service('repository/session'),
   coordinateRepo: service('repository/coordinate'),
-  serviceRepo: service('repository/service'),
   model: function(params) {
     const dc = this.modelFor('dc').dc.Name;
     const nspace = this.modelFor('nspace').nspace.substr(1);
@@ -16,11 +14,9 @@ export default Route.extend({
       item: this.repo.findBySlug(name, dc, nspace),
       sessions: this.sessionRepo.findByNode(name, dc, nspace),
       tomography: this.coordinateRepo.findAllByNode(name, dc),
-      services: this.serviceRepo.findBySlug(params.name, dc, nspace),
     });
   },
   setupController: function(controller, model) {
-    console.log(get(model, 'item.content'));
     controller.setProperties(model);
   },
 });
